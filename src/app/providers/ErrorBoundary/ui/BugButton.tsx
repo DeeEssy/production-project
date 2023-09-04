84eb11424b6264e02a15d8b0deb026786a033671
Shevchenko Dmytro <venchesters.2002@gmail.com>
2023-09-05 00:40:09 +0300

    add errorBoundary

---
+++ src/app/providers/ErrorBoundary/ui/BugButton.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from 'react';
+import { Button } from 'shared/ui/Button/Button';
+
+// Component only for testing
+export const BugButton = () => {
+  const [error, setError] = useState(false);
+
+  const toggleError = () => setError((prev) => !prev);
+
+  useEffect(() => {
+    if (!error) return;
+
+    throw new Error();
+  }, [error]);
+
+  return (
+    <div>
+      {/* eslint-disable-next-line i18next/no-literal-string */}
+      <Button onClick={toggleError}>create error</Button>
+    </div>
+  );
+};
---
+++ src/widgets/ErrorBlock/ui/ErrorBlock.tsx
@@ -0,0 +1,23 @@
+import { classNames } from 'shared/lib';
+import { useTranslation } from 'react-i18next';
+import { Button } from 'shared/ui/Button/Button';
+import cls from './ErrorBlock.module.scss';
+
+interface ErrorBlockProps {
+  className?: string;
+}
+
+export const ErrorBlock = ({ className }: ErrorBlockProps) => {
+  const { t } = useTranslation();
+
+  const reloadPage = () => {
+    window.location.reload();
+  };
+
+  return (
+    <div className={classNames(cls.ErrorBlock, {}, [className])}>
+      <p>{t('error_block_text')}</p>
+      <Button onClick={reloadPage}>{t('error_block_btn')}</Button>
+    </div>
+  );
+};
