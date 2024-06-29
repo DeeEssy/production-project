34a8ebe410ea6e254109e1d7b75eef8ddab92f3b
Shevchenko Dmytro <venchesters.2002@gmail.com>
2024-06-29 11:47:50 +0300

    added react-virtualized

--- src/widgets/Page/Page.tsx
+++ src/widgets/Page/Page.tsx
@@ -20,6 +20,8 @@ interface PageProps {
 
 const SCROLL_CHECKER_DELAY = 1000;
 
+export const PAGE_ID = 'PAGE_ID';
+
 export const Page = memo((props: PageProps) => {
   const { className, children, onScrollEnd } = props;
   const wrapperRef = useRef() as MutableRefObject<HTMLDivElement>;
@@ -47,6 +49,7 @@ export const Page = memo((props: PageProps) => {
       ref={wrapperRef}
       className={classNames(cls.page, {}, [className])}
       onScroll={onScroll}
+      id={PAGE_ID}
     >
       {children}
       {onScrollEnd ? <div className={cls.trigger} ref={triggerRef} /> : null}
