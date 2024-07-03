a789582f920e33523b2313156d7953956101f30e
Shevchenko Dmytro <venchesters.2002@gmail.com>
2024-07-03 14:27:16 +0300

    added html report for unit tests

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -36,6 +36,16 @@ export default {
   },
   globals: { __IS_DEV__: true, __API__: '', __PROJECT__: 'jest' },
 
+  reporters: [
+    'default',
+    ['jest-html-reporters', {
+      publicPath: '<rootDir>/reports/unit',
+      filename: 'report.html',
+      openReport: true,
+      inlineSource: true,
+    }],
+  ],
+
   // Indicates whether the coverage information should be collected while executing the test
   // collectCoverage: false,
 
